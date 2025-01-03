270d0b33c24c51805bbbd1816ca3f1c2cf4862c2
AnonComrade993 <anoncomrade993@gmail.com>
2025-01-03 21:31:56 +0100

    update

--- src/public/scripts/quoter.js
+++ src/public/scripts/quoter.js
@@ -1,25 +1,25 @@
 document.addEventListener('DOMContentLoaded', () => {
-
 	const quoteContainer = document.getElementById('quote-container');
 
 	const quoteHandler = async () => {
 		try {
 			const response = await fetch('/api/quote');
-
 			const quoteData = await response.json();
 
 			if (quoteData.success) {
 
-				quoteContainer.text = quoteData.data;
+				quoteContainer.textContent = quoteData.data;
 			} else {
-				const errorMessage = await getErrorMessage(response);
-				throw errorMessage;
+				const errorMessage = quoteData.error || 'Failed to fetch quote';
+				throw new Error(errorMessage);
 			}
 		} catch (error) {
 			console.error('Network error:', error);
-			throw error
+			quoteContainer.textContent = 'Failed to load quote';
 		}
 	}
 
+
+	quoteHandler();
 	setInterval(quoteHandler, 5000);
 });
