ed717a99e4a8856f996f4d876067ed10b487c5b5
AnonComrade993 <anoncomrade993@gmail.com>
2025-01-03 22:04:29 +0100

    update viewRouters.js

--- src/routers/viewRouters.js
+++ src/routers/viewRouters.js
@@ -14,8 +14,8 @@ let links = {
 	start_url: process.env.START_URL,
 	inventions: 'https://teslasciencecenter.org/nikola-tesla-inventions/',
 	laboratory: 'https://en.wikipedia.org/wiki/Tesla_Experimental_Station',
-	philosophy: 'https://medium.com/@mashrur_ayon/nikola-tesla-his-core-philosophy-and-its-implications-on-modern-science-98e84bfadd03'
-discover: 'https://en.wikipedia.org/wiki/Nikola_Tesla'
+	philosophy: 'https://medium.com/@mashrur_ayon/nikola-tesla-his-core-philosophy-and-its-implications-on-modern-science-98e84bfadd03',
+	discover: 'https://en.wikipedia.org/wiki/Nikola_Tesla'
 };
 
 
